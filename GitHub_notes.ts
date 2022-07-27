7a1f7b16d2bc2988c2e6733718cb87a7d9753503
Vishal Umakant Pawar <110019702+vpawar-argusoft@users.noreply.github.com>
2022-07-27 20:44:39 +0530

    notes added

--- GitHub_notes.ts
+++ GitHub_notes.ts
@@ -1,7 +1,28 @@
 // This are my notes on GitGitHub Master class
 
+Section 1 - Getting started with Git
+
 git config --global user.name "Vishal Pawar"          // configure name for automation
 git config --global user.email "vpawar@argusoft.com"    // configure email id
+git config --global core.editor "code --wait"  // To configure main code editor   
+
+  8. Executing a first commit 
+    1. create repository frist by changing directory -- cd -- where we need to create files
+    2. create needed file exa. index.html
+    3. make respective folder as git folder -> get init // by initialising git 
+    4. by using (ls -a ) // we can see hidden+non hidden files for respective loation
+    5. git add index.html // To add file in repo
+    6. git commit -m "given message " // commit by adding message 
+
+  10. Reviewing file changes by (git diff index.html)
+
+  
+
+
+Section 2 - Getting started with GitHub
+
+12. Diff in git and GitHub
+13. Create your account    
 
 git remote add origin https://github.com/vpawar-argusoft/GitGithubMasterclass.git  // Add folder to GitHUB
 
@@ -12,14 +33,16 @@ git commit -m "Add typescript file" // Commit file added to Github
 
 git push origin master // to push data from git to github
 
+15. Editing code and commiting from gitHUb
 
-// To configure main code editor
-git config --global core.editor "code --wait"        
+16. Pulling from remote 
 
-// This command is used to pull data from GitHub
-git pull origin master
+git pull origin master // This command is used to pull data from GitHub
 
-// This command is to automatically pull data from GitHub 
-git branch --set-upstream-to=origin/master master
-//  Now only use
-git pull
+git branch --set-upstream-to=origin/master master. // This command is to automatically pull data from GitHub 
+
+git pull  //  Now only use instead of above 2 commands
+
+git fetch //sync from github to local git
+git status //check status after sync
+git pull 
