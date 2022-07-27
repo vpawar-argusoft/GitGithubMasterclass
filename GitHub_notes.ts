dcee0a05c4e6172321ef60c6af965c5596f90372
vpawar-argusoft <vpawar@argusoft.com>
2022-07-27 19:40:24 +0530

    Add typescript file

---
+++ GitHub_notes.ts
@@ -0,0 +1,16 @@
+// This are my notes on GitGitHub Master class
+
+// to push data from git to github
+git push origin master
+
+
+// To configure main code editor
+git config --global core.editor "code --wait"        
+
+// This command is used to pull data from GitHub
+git pull origin master
+
+// This command is to automatically pull data from GitHub 
+git branch --set-upstream-to=origin/master master
+//  Now only use
+git pull
