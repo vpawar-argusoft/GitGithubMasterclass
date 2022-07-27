d08ace2d02f8cc379cb93d598dbe3ce37b433a84
Vishal Umakant Pawar <110019702+vpawar-argusoft@users.noreply.github.com>
2022-07-27 20:10:01 +0530

    Update GitHub_notes.ts

--- GitHub_notes.ts
+++ GitHub_notes.ts
@@ -14,3 +14,7 @@ git pull origin master
 git branch --set-upstream-to=origin/master master
 //  Now only use
 git pull
+
+git fetch // after editing in gitHUb to update in local git folder we need to fetch 
+git status // get the status of local git repository what needed to do next
+git pull // then we can pull data from github
