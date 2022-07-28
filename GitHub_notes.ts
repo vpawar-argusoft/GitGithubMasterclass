fd5641dd80a74bfa3428873fb34ea0549b7c5558
Vishal Umakant Pawar <110019702+vpawar-argusoft@users.noreply.github.com>
2022-07-28 17:27:25 +0530

    updated notes of 4. working with branches

--- GitHub_notes.ts
+++ GitHub_notes.ts
@@ -24,33 +24,33 @@ git config --global core.editor "code --wait"  // To configure main code editor
 Section 2 - Getting started with GitHub
 
 
-12. Diff in git and GitHub
-13. Create your account    
+    12. Diff in git and GitHub
+    13. Create your account    
 
-git remote add origin https://github.com/vpawar-argusoft/GitGithubMasterclass.git  // Add folder to GitHUB
+    git remote add origin https://github.com/vpawar-argusoft/GitGithubMasterclass.git  // Add folder to GitHUB
 
-git add GitHub_notes.ts  // Add file to github
-git commit -m "Add typescript file" // Commit file added to Github
+    git add GitHub_notes.ts  // Add file to github
+    git commit -m "Add typescript file" // Commit file added to Github
 
-14. Push Your local repo to Github
+    14. Push Your local repo to Github
 
-git push origin master // to push data from git to github
+    git push origin master // to push data from git to github
 
-15. Editing code and commiting from gitHUb
+    15. Editing code and commiting from gitHUb
 
-16. Pulling from remote 
+    16. Pulling from remote 
 
-git pull origin master // This command is used to pull data from GitHub
+    git pull origin master // This command is used to pull data from GitHub
 
-git branch --set-upstream-to=origin/master master. // This command is to automatically pull data from GitHub 
+    git branch --set-upstream-to=origin/master master. // This command is to automatically pull data from GitHub 
 
-git pull  //  Now only use instead of above 2 commands
+    git pull  //  Now only use instead of above 2 commands
 
-17. Check changes with git status 
+    17. Check changes with git status 
 
-git fetch //sync from github to local git
-git status //check status after sync
-git pull 
+    git fetch //sync from github to local git
+    git status //check status after sync
+    git pull 
 
 
 
@@ -79,5 +79,37 @@ Section 3 - Fork and clone
   3. git checkout 284cdd63 // to revert particular commit
   4. git add . // will add all modified files to git
   5. git checkout -- .  //will revert all recent changes
+  
+  
+  
+  
+Section 4. Working with branches 
+  25. Good version control practices - (dev->test->master)
+  26. What is a branch? 
+  27. Create a branch with git 
+    1. git branch dev //creates dev branch
+    2. git branch  // shows how many and what branches are in given repo and which is selected (shows * in prefix)
+    3. git checkout dev // selects given branch
+    4. git checkout -b add-bootsrtrap  // creates new branch + selects it 
+  
+  28. create a branch with github
+  29. adding a gitignore file -> This ignore given folder/files and not include it in Github even it is present in local git repos 
+    1. git add .gitignore //first add gitignore file in respective location
+      1. /index.html // ignore given file
+      2. text/ // ignore folder including all files in it
+      3. text/*.txt // ignore only .txt files in folder
+      
+  30. Syncing branches 
+    1. git checkout --track origin/test // setup to track remote test branch (added + switched to it)
+    2. git checkout --track origin/<branch name> // it adds + selects branch from remote to local + tracks it
+    
+  40. Graph the branches on terminal
+    1. git log --graph // checks how ,when branches merged
+    
+  41. Merging branches in terminal
+    1. git merge <branch name which tobe addded> <branch name where prevous branch is being added> --> git merge a b
+    2. git branch -d add-bootstrap // deletes given branch
+    
+  42. Pull request in Github
   
 
